d9ff30d31fcc9be8ea6bdf74615203544ccb8aeb
thisisdosho <mohameddosho20@gmail.com>
2026-04-02 16:59:53 +0300

    feat: add drone importer support (LAS/LAZ point clouds, Pix4D CSV/XML)

--- src/lib/importers/index.ts
+++ src/lib/importers/index.ts
@@ -4,6 +4,10 @@ import './parsers/jobxml';
 import './parsers/trimbleRw5';
 import './parsers/dxf';
 import './parsers/traverse-csv';
+import './parsers/las';
+import './parsers/pix4d';
 
 export { detectFormat, getParser, getAllParsers } from './registry';
 export type { Parser } from './registry';
+export { DRONE_EXTENSIONS, type ParsedSurveyData, type ParserFunction } from './droneTypes';
+export { normalizeToEngine } from './normalize';
